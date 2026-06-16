4d3fcc646e035f0625c49d0321bdd8c26831d151
LozengeDrop <lozengedropdesign@gmail.com>
2026-06-16 16:42:44 -0600

    Delete unused script

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-npm exec ergogen -- .
-cp output/pcbs/chouchou.kicad_pcb kicad/
-# for i in output/cases/*.jscad; do npm exec openjscad -- "$i" -of stla; done
-npm exec openjscad -- output/cases/case.jscad -of stla -o case/case.stl
